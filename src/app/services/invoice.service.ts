57c0db16ee34db8993e422a6af712f3201df31d0
Kevin Vanthuyne <kevin.vanthuyne2@gmail.Com>
2019-08-21 21:12:09 +0200

    added invoice id handling and saving to backend

--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+import { environment } from 'src/environments/environment';
 import { Invoice } from '../models/Invoice';
+import { JsonResponse } from '../models/jsonResponse';
 
 @Injectable({
   providedIn: 'root',
@@ -8,13 +12,24 @@ import { Invoice } from '../models/Invoice';
 export class InvoiceService {
   invoice: Invoice;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   get(): Invoice {
     return this.invoice;
   }
 
+  getNextId(): Observable<JsonResponse> {
+    return this.http.get<JsonResponse>(`${environment.apiUrl}/invoices/nextId`);
+  }
+
   save(invoice: Invoice): void {
     this.invoice = invoice;
+  }
+
+  saveToBackend(): Observable<JsonResponse> {
+    return this.http.post<JsonResponse>(
+      `${environment.apiUrl}/invoices`,
+      this.invoice
+    );
   }
 }
